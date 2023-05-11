a615eee3a08ffefa41c12d434fb9256b8ce5a618
Никита Шинков <shinkovn@yandex.ru>
2023-05-11 14:59:04 +0400

    test7

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
-from django.db import IntegrityError, models
+from django.db import IntegrityError
+from django.db.models import Avg
 from django.shortcuts import get_object_or_404
-
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -14,8 +14,6 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 from api.mixins import CustomSet
 from reviews.models import Category, Genre, Title, User
-
-
 from .filters import TitleFilter
 from .paginator import CommentPagination
 from .permissions import (IsAdmin, IsAdminOrReadOnly,
